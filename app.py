f3f7cf9871e91dfb6e838d5c22d1cec4f0421db2
Teymur <teymur_gahramanov@gmail.com>
2024-07-08 12:12:51 +0400

    it works

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from kubernetes import client, config
 import requests
 
 app = Flask(__name__)
@@ -9,28 +10,49 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    api_urls = [
-        "http://localhost:8080/probe"
-        # Add more URLs as needed
-    ]
-
     dropdown_value = request.form['dropdown']
     text_input = request.form['text_input']
+    timeout = int(request.form.get('timeout', 15))
     results = []
 
-    for url in api_urls:
+    for exporter in exporters.values():
         data = {
-            "module": dropdown_value,
-            "address": text_input
+            "dropdown_value": dropdown_value,
+            "text_input": text_input,
+            "timeout": timeout
         }
-        response = requests.post(url, json=data)
-        response_data = response.json()
+        try:
+            response = requests.post(exporter["api_url"], json=data, timeout=10)
+            response_data = response.json()
+            result = response_data.get("result", False)
+            if "error" in response_data:
+                result = response_data["error"]
+        except requests.exceptions.RequestException as e:
+            result = str(e)
+
         results.append({
-            "api_url": url,
-            "result": response_data.get("result", False)
+            "host": exporter["host"],
+            "result": result
         })
 
     return render_template('result.html', results=results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    exporters = {}
+    # Load the kubeconfig file
+    config.load_kube_config()
+
+    # Create an instance of the API class
+    v1 = client.CoreV1Api()
+
+    # Define the label selector
+    label_selector = "app=from-node-exporter"
+
+    # List all pods with the specified label
+    pods = v1.list_pod_for_all_namespaces(label_selector=label_selector)
+
+    # Iterate through the pods and print the pod name and host IP
+    for pod in pods.items:
+        exporters[pod.metadata.name] = {"host":f"{pod.status.host_ip}", "api_url": f"http://{pod.metadata.name}:8080/probe"}
+    print(exporters)
+    app.run(debug=False)
