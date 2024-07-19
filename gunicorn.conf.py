8a9c600a80c2c51507f59a5a5b658b7b4474c2f2
Teymur Gahramanov <teymur_gahramanov@outlook.com>
2024-07-19 18:36:23 +0400

    set default workers to 2

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 import os
 
-workers = int(os.getenv('GUNICORN_WORKERS', '4'))
+workers = int(os.getenv('GUNICORN_WORKERS', '2'))
 worker_class = os.getenv('GUNICORN_WORKER_CLASS', 'gevent')
 bind = os.getenv('GUNICORN_BIND', '0.0.0.0:8000')
 timeout = int(os.getenv('GUNICORN_TIMEOUT', '30'))
